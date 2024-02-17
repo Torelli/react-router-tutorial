34f3ca3d97f0a7e05c60147a0ac6e5d464568a72
Giovanni Torelli <giovanni.walter@outlook.com>
2024-02-17 17:44:37 -0300

    Created optimistic UI for better user experience

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -69,6 +69,7 @@ function Favorite({ contact }: { contact: ContactType }) {
   const fetcher = useFetcher();
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+  if (fetcher.formData) favorite = fetcher.formData.get("favorite") === "true";
   return (
     <fetcher.Form method="post">
       <button
