695ed9499712b98292ae3021995cee9612fee260
Giovanni Torelli <giovanni.walter@outlook.com>
2024-02-17 17:47:55 -0300

    created throwable error for non existing data

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -3,6 +3,12 @@ import { ContactType, getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
