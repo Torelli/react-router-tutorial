6abcdbc9a216d32508d1e578a77bed829faf68cb
Giovanni Torelli <giovanni.walter@outlook.com>
2024-02-17 16:26:34 -0300

    feat: created delete contact feature

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import Root, {
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -27,7 +28,11 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
-        action: editAction
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
